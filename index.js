3c0aecccea9b94e777d1f577328914a23e5e2172
DauMoe <47239428+DauMoe@users.noreply.github.com>
2019-07-14 18:34:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-google.charts.load('current', {'packages':['corechart']});
+//Google Chart
+google.charts.load('current', {'packages': ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Year', 'Sales', 'Expenses'],
-        ['2004',  1000,      400],
-        ['2005',  1170,      460],
-        ['2006',  660,       1120],
-        ['2007',  1030,      540]
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
     ]);
 
     var options = {
         title: 'Báo cáo công việc',
         curveType: 'function',
-        legend: { position: 'bottom' }
+        legend: {position: 'bottom'}
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('chart'));
 
     chart.draw(data, options);
+}
+
+//AJAX load table nhan vien
+function load_table_nv() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("table").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "table_nv.html", true);
+    xhttp.open("GET", "table_nv.html", true);
+    xhttp.send();
+}
+
+//AJAX load table cong viec
+function load_table_cv() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("table").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "table_cv.html", true);
+    xhttp.send();
 }
